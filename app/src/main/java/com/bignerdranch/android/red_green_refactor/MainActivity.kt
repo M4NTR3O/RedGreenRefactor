eb9eef87f4c86974d127a24ea0ce25440ad15d3a
emil_ <emil.kushner.stud@mail.ru>
2024-10-14 12:39:49 +0700

    Ready function getSearchUrl

--- app/src/main/java/com/bignerdranch/android/red_green_refactor/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/red_green_refactor/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.red_green_refactor
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,23 +11,35 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var resultText: TextView
+    private lateinit var resultText2: TextView
+    private lateinit var button: Button
+    private lateinit var editText: EditText
+    private lateinit var resultFunction: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         resultText = findViewById(R.id.returnResult1Test)
+        resultText2 = findViewById(R.id.returnResult2Test)
+        button = findViewById(R.id.buttonInput)
+        editText = findViewById(R.id.editTextQuery)
+        resultFunction = findViewById(R.id.textViewResultFunction)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        firstTest()
+        button.setOnClickListener {
+            firstTest()
+            secondTest(editText.text.toString())
+            resultFunction.text = getSearchUrl(editText.text.toString())
+        }
     }
     fun getSearchUrl(query: String?): String?{
         if (query == null){
             return null
         }
-        return ""
+        return "https://www.google.com/search?q=$query/"
     }
     fun firstTest(){
 
@@ -34,6 +48,14 @@ class MainActivity : AppCompatActivity() {
         }
         else{
             resultText.text = "Not success"
+        }
+    }
+    fun secondTest(query: String?){
+        if (getSearchUrl(query) != null){
+            resultText2.text = "Success"
+        }
+        else{
+            resultText2.text = "No success"
         }
     }
 }
